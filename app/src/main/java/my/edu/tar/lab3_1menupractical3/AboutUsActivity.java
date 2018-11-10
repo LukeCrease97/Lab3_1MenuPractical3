b71d15a769878a8f5906b058185a22a406950913
LukeCrease97 <hengyh-wa16@student.tarc.edu.my>
2018-11-10 14:52:30 +0800

    Initial

---
+++ app/src/main/java/my/edu/tar/lab3_1menupractical3/AboutUsActivity.java
@@ -0,0 +1,70 @@
+package my.edu.tar.lab3_1menupractical3;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+    public void visitTARUC(View view)
+    {
+        //todo: create implicit Intent
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.tarc.edu.my"));
+        if(intent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "No app to handle this", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void showmain(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        startActivity(intent);
+    }
+
+    public void showSentTo(View view)
+    {
+        //todo: create implicit Intent
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + "info@tarc.edu.my"));
+        if(intent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "No app to handle this", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showDial(View view)
+    {
+        //todo: create implicit Intent
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + "0395433336"));
+        if(intent.resolveActivity(getPackageManager())!=null)
+        {
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "No app to handle this", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+}
